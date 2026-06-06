bc578cf1fda6dcf0d8c26f5afaae4a9e3591426d
asm-saim <saim777m@gmail.com>
2026-06-06 08:02:32 +0600

    login input successfully configured

--- script.js
+++ script.js
@@ -2,6 +2,22 @@
 document.getElementById('loginButton').addEventListener('click',
     function (e) {
         e.preventDefault() //to prevent by default submit button behavior
-        console.log('button clicked');
-        console.log(e);
+
+        const mobileNumber = 10987654321;
+        const mobilePin = 4321;
+        //get mobileNumber:
+        const catchMobileNumber = document.getElementById('mobile-number').value;
+        const newMobileNumber = parseInt(catchMobileNumber);
+        //get pinNumber:
+        const catchPinNumber = document.getElementById('pin-number').value;
+        const newPinNumber = parseInt(catchPinNumber);
+
+        //condition
+        if(newMobileNumber===mobileNumber && newPinNumber===mobilePin)
+        {
+            window.location.href='./home.html'
+        }
+        else{
+            alert('Invalid Credential')
+        }
     })
